f056f7a74d0170f95cb49c13d89d554b511c1697
Potoxela <alexotop@live.fr>
2024-05-10 00:42:58 +0200

    Start
    Premières classes

---
+++ src/Point.java
@@ -0,0 +1,21 @@
+public class Point {
+    
+    private double x;
+    private double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
