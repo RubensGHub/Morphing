3757e51bc1743c139387ac12d11b57d7cce4dfd9
RomainCrrl <169135012+RomainCrrl@users.noreply.github.com>
2024-05-21 10:57:05 +0200

    Update ControleImgEnd.java

--- src/controle/ControleImgEnd.java
+++ src/controle/ControleImgEnd.java
@@ -1,5 +1,30 @@
 package controle;
 
-public class ControleImgEnd {
-    
+import java.util.Observable;
+import java.util.Observer;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import morphing.MorphingApp;
+
+public class ControleImgEnd implements Observer{
+protected Button bouton;
+	
+	protected ImageView ivStart = new ImageView();
+	protected MorphingApp app;
+	
+	public ControleImgEnd(MorphingApp app, ImageView ivStart) {
+		this.ivStart = ivStart;
+		this.app = app;
+
+	}
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		// Création de ImageView pour affichage
+				Image image = SwingFXUtils.toFXImage(app.getImgSrc().getImage(), null);
+		        ivStart.setImage(image);		
+	}
 }
