109098efaf9649f0cf68d2fa2f6921ca53c4ba6a
RomainCrrl <169135012+RomainCrrl@users.noreply.github.com>
2024-05-21 10:52:45 +0200

    Update ControleImgStart.java

--- src/controle/ControleImgStart.java
+++ src/controle/ControleImgStart.java
@@ -1,32 +1,49 @@
 package controle;
-import java.util.Observable;
+import morphing.*;
+
 import java.util.Observer;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.scene.control.Button;
-import javafx.scene.image.*;
+import javax.imageio.ImageIO;
+
+import java.util.Observable;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
 import javafx.stage.FileChooser;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 
-@Deprecated
-public class ControleImgStart implements Observer, ChangeListener<Number> {
-    
-    private ImageView ivStart;
-    private Button addFile;
-    private FileChooser fileChooser = new FileChooser();
+public class ControleImgStart implements Observer {
+	
+	protected Button bouton;
+	
+	protected ImageView ivStart = new ImageView();
+	protected MorphingApp app;
+	
+	public ControleImgStart(MorphingApp app, ImageView ivStart) {
+		this.ivStart = ivStart;
+		this.app = app;
 
-    public ControleImgStart(Button addFile, ImageView ivStart) {
-        this.ivStart = ivStart;
-        this.addFile = addFile;
-    }
+	}
+
+
 
 	@Override
 	public void update(Observable o, Object arg) {
-        
-	}
+		// Création de ImageView pour affichage
+		Image image = SwingFXUtils.toFXImage(app.getImgSrc().getImage(), null);
+        ivStart.setImage(image);
+    }
+	
+	
+	
+	
+	
 
-	@Override
-	public void changed(ObservableValue<? extends Number> observable, Number value1, Number value2) {
-       	
-	}
+
 }
