861db502c858ea80aa59a5415ca4750f0d732f6b
RomainCrrl <169135012+RomainCrrl@users.noreply.github.com>
2024-05-23 10:23:59 +0200

    Add files via upload

---
+++ src/controle/ControleBoutonBeier.java
@@ -0,0 +1,37 @@
+package controle;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Observable;
+import java.util.Observer;
+import javax.imageio.ImageIO;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.FileChooser;
+import morphing.*;
+
+@SuppressWarnings("deprecation")
+public class ControleBoutonBeier implements Observer, EventHandler<ActionEvent> {
+    private MorphingApp app;
+
+
+    public ControleBoutonBeier(MorphingApp app){
+        this.app = app;
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        /*try {
+            CourbesBezier();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        
+    }
+}
